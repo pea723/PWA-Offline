1b077415641694a8b67df574e0fc57dc73d1b55f
natmatsumoto <pea723@gmail.com>
2018-05-15 00:47:16 +0900

    style fix for iphone

--- scripts/reader.js
+++ scripts/reader.js
@@ -1,9 +1,10 @@
 
 $(function() {
+  // テキスト取得
   var xhr = new XMLHttpRequest();
   var right = 0;
-  var width = window.innerWidth;
-  console.log(width);
+  var width = window.innerWidth - 13;
+  var wrap_w = 0;
 
   xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
@@ -19,45 +20,47 @@ $(function() {
     text = text.split('\n');
     text.forEach(function(e) {
       $('<p>&nbsp'+e+'</p>').appendTo('#wrapper');
+       wrap_w = $('#wrapper').width()
     });
   }
 
-  $('body').on('touchstart', onTouchStart); //指が触れたか検知
-  $('body').on('touchmove', onTouchMove); //指が動いたか検知
-  $('body').on('touchend', onTouchEnd); //指が離れたか検知
-  var direction, position;
+  // スワイプ判定
+  var _dir, position;
 
-  //スワイプ開始時の横方向の座標を格納
-  function onTouchStart(event) {
-    position = getPosition(event);
-    direction = ''; //一度リセットする
-  }
-
-  //スワイプの方向（left／right）を取得
-  function onTouchMove(event) {
-    if (position - getPosition(event) > 70) { // 70px以上移動しなければスワイプと判断しない
-      direction = 'left'; //左と検知
-    } else if (position - getPosition(event) < -70){  // 70px以上移動しなければスワイプと判断しない
-      direction = 'right'; //右と検知
-    }
-  }
-
-  function onTouchEnd(event) {
-    if (direction == 'right'){
-      if (right > -$('#wrapper').width()) {
-        right -= width;
-        $('#wrapper').animate({'right': right}, 500);
-      }
-    } else if (direction == 'left'){
-      if (right <= -width) {
-        right += width;
-        $('#wrapper').animate({'right': right}, 500);
-      }
-    }
-  }
-
-  //横方向の座標を取得
-  function getPosition(event) {
+  function getPos(event) {
     return event.originalEvent.touches[0].pageX;
   }
+
+  function move(dir) {
+    if (dir == 'right' && right <= -width) {
+      right += width;
+    } else if (dir == 'left' && right >= -wrap_w+width*2) {
+      right -= width;
+    }
+    _dir = '';
+    $('#wrapper').animate({'right': right}, 500);
+  }
+
+  $('body').on('touchstart', function(event) {
+    pos = getPos(event);
+  });
+  $('body').on('touchmove', function(event) {
+    if (pos - getPos(event) > 50) {
+      _dir = 'right';
+    } else if (pos - getPos(event) < -50) {
+      _dir = 'left';
+    }
+  });
+  $('body').on('touchend', function() {
+    move(_dir);
+  });
+
+  // サイドのコントローラー
+  $('#left').on('click', function() {
+    move('left');
+  });
+  $('#right').on('click', function() {
+    move('right');
+  });
+
 });
