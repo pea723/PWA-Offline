9de9004a9c69a9db299599ae9e2e4291aec835d0
natmatsumoto <pea723@gmail.com>
2018-05-14 22:20:18 +0900

    fix

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
       return cache.addAll([
         '/',
         '/service-worker.js',
-        '/index.html',
+        '/home.html',
         '/manifest.json',
         '/css/style.css',
         '/data/chumon.txt',
